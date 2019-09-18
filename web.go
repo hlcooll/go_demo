ffd7512bc0fa3fd71fc4a086c3de464ded2a28c9
hualei <michaellei@126.com>
2019-09-18 20:10:19 +0800

    修改web_prometheus

--- web.go
+++ web.go
@@ -1,29 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
 	"log"
 	"net/http"
-	"strings"
+
+	_ "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       //解析参数，默认是不会解析的
-	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
+func sayhelloName11(w http.ResponseWriter, r *http.Request) {
+	//r.ParseForm()       //解析参数，默认是不会解析的
+	//fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
+	//fmt.Println("path", r.URL.Path)
+	//fmt.Println("scheme", r.URL.Scheme)
+	//fmt.Println(r.Form["url_long"])
+	//for k, v := range r.Form {
+	//	fmt.Println("key:", k)
+	//	fmt.Println("val:", strings.Join(v, ""))
+	//}
+	//fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
+
+	w.Write([]byte(`<html>
+             <head><title>RabbitMQ Exporter</title></head>
+             <body>
+             <h1>RabbitMQ Exporter</h1>
+             <p><a href='/metrics'>Metrics</a></p>
+             </body>
+             </html>`))
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)       //设置访问的路由
+	http.Handle("/metrics", prometheus.Handler())
+	http.HandleFunc("/", sayhelloName11)     //设置访问的路由
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+
 }
